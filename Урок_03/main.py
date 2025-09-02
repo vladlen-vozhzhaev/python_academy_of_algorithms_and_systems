53353217a5c52189cc8c153b00d51ff84ce86c7d
Vlalden <vladlen@vozhzhaev.ru>
2025-09-02 19:06:46 +0300

    add lesson 3-4

---
+++ Урок_03/main.py
@@ -0,0 +1,29 @@
+a = input("введите число ")
+b = input("введите число ")
+print(float(a)+float(b))
+
+# name = input("Введите имя:")
+# #print("Hello "+name)
+# print(f"Привет {name}")
+# x = 5
+# y = 3
+# print(x + y) # 8
+# print(x - y) # 2
+# print(x * y) # 15
+# print(x / y) # 1.6
+# print(x % y) # 2
+# print(x // y) # 1
+# print(x ** y) # 125
+#
+# print(x == y)
+# print(x != y)
+# print(x < y)
+# print(x > y)
+# print(x <= y)
+# print(x >= y)
+
+# x = True
+# y = False
+# print(x and y)
+# print(x or y)
+# print(not x)
