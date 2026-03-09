53fa9eb4c5b4577a3bd7f17ab38ab56b38b58ea0
Vlalden <vladlen@vozhzhaev.ru>
2026-03-09 22:28:01 +0300

    add lesson 010

---
+++ Урок_010/dictionaries.py
@@ -0,0 +1,57 @@
+users = {
+    "user1": {"name": "Alice", "age": 25},
+    "user2": {"name": "Bob", "age": 30}
+}
+
+print(users["user1"]["name"])
+print(users["user2"]["age"])
+
+
+# my_dict = {
+#     "name":"Alice",
+#     "age": 25,
+#     "city": "New York"
+# }
+#
+# for key in my_dict:
+#     print(key)
+# for value in my_dict.values():
+#     print(value)
+# for key, value in my_dict.items():
+#     print(f"{key}: {value}")
+#
+# squares = {x: x**2 for x in range(1,6)}
+# print(squares)
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.items())
+# my_dict.update({"age":26, "country": "USA"})
+# print(my_dict)
+# print(my_dict["name"])
+# print(my_dict["age"])
+#
+# print(my_dict.get("city"))
+#
+# print(my_dict.get("country", "Unknown"))
+# my_dict["age"] = 26
+# my_dict["country"] = "USA"
+# print(my_dict)
+#
+# del my_dict["age"]
+# print(my_dict)
+#
+# city = my_dict.pop("city")
+# print(city)
+# print(my_dict)
+#
+# list_item = my_dict.popitem()
+# print(list_item)
+# print(my_dict)
+#
+# my_dict.clear()
+# print(my_dict)
+# print(my_dict["country"])
+
+
+# my_dict2 = dict(name="Alice", age=25, city="New York")
+# print(my_dict2)
