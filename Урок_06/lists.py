b7b2534da415bf8de3c537ce2e882eeaf47b2bb6
Vlalden <vladlen@vozhzhaev.ru>
2026-03-09 22:00:28 +0300

    add lesson 6

---
+++ Урок_06/lists.py
@@ -0,0 +1,47 @@
+my_list = ["яблоки", "груши", "апельсины", "груши"]
+numbers = [4,2,62,765,23,6]
+print( max(numbers) )
+print( min(numbers) )
+print( sum(numbers) )
+print( len(numbers) )
+# condition = lambda x: x%2 == 0
+# new_numbers = [item for item in numbers if condition(item)]
+# for item in numbers:
+#     if item % 2 == 0:
+#         new_numbers.append(item)
+# print(new_numbers)
+# new_numbers = [item*2 for item in numbers]
+# for item in numbers:
+#     new_numbers.append(item*2)
+# print(new_numbers)
+# new_numbers = sorted(numbers)
+# reverese_numbers = reversed(numbers)
+# new_numbers2 = numbers.copy()
+# print(numbers)
+# print(new_numbers)
+# print(new_numbers2)
+# for item in reverese_numbers:
+#     print(item)
+
+# print( my_list.count("ананасы") )
+# print( my_list.index("груши") )
+# print( numbers.index(765) )
+
+# my_list.append("бананы")
+# my_list.insert(1, "бананы")
+# my_list.sort()
+# my_list.reverse()
+# # my_list.remove("груши")
+# # my_list.pop(0)
+# # my_list.clear()
+# # print(my_list)
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])
+# print(my_list[4])
+# # my_list[0] = "бананы"
+# # print(my_list[0])
+# numbers.sort()
+# numbers.reverse()
+# print(numbers)
---
+++ Урок_06/strings.py
@@ -0,0 +1,41 @@
+# multiline = """строка
+# строка
+# строка"""
+# print(multiline)
+# path = "C:\\Users\\Documents\\file.txt"
+
+original_string = "I like apples"
+print(len(original_string))
+# original_string = "I like apples"
+# position = original_string.find("apples")
+# print(position)
+# new_string = original_string.replace("apples", "oranges")
+# print(new_string)
+
+# path = "example.com/user/342"
+# print(path.split("/"))
+# my_string = "Hello world!"
+# print(my_string.split())
+# print(my_string)
+# print(my_string.strip())
+
+# lower_text = my_string.lower()
+# upper_text = my_string.upper()
+# title_text = my_string.title()
+# print(lower_text)
+# print(upper_text)
+# print(title_text)
+# email = "Test@example.com"
+# user_login = "test@example.com"
+# if "oo" in my_string:
+#     print("Подстрока найдена")
+# else:
+#     print("Подстрока не найдена")
+
+# first_char = my_string[0]
+# last_char = my_string[-1]
+# print(first_char)
+# print(last_char)
+#
+# substring = my_string[6:11]
+# print(substring)
