95570fdd606b341b8b9f9c0331ef4be8d2d18164
Vlalden <vladlen@vozhzhaev.ru>
2026-03-09 21:52:35 +0300

    add lesson 5

---
+++ Урок_05/cycles.py
@@ -0,0 +1,54 @@
+for number in range(0,10):
+    if number % 2 == 0:
+        continue
+    print(number)
+
+
+# for i in range(1,11):
+#     for j in range(1, 11):
+#         print(f"{i}*{j} = {i*j}")
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+# marks = [4,4,5,5,3,4,4]
+#
+# sum = 0
+# for mark in marks:
+#     sum += mark
+#
+# result = sum/len(marks)
+# print(result)
+
+# while True:
+#     login = input("Введите логин: ")
+#     password = input("Введите пароль: ")
+#
+#     if login == "admin" and password == "123":
+#         print("Доступ разрешен для ADMIN")
+#         break
+#     elif login == "user" and password == "321":
+#         print("Доступ разрешен для USER")
+#         break
+#     else:
+#         print("Доступ запрещен")
+#         print("Попробуйте ещё раз")
+
+# i = 1
+# while True:
+#     print(i)
+#     i += 1 # i = i + 1
+
+
+# lang = "Python"
+#
+# for char in lang:
+#     print(char)
+
+
+# numbers = [1,2,3,4,5,6]
+# for number in numbers:
+#     print(number)
